483fa901c81876db1da2ae3a1310a0e0aac89df2
Jennifer Mitchell <jennifer.mitchell@utah.edu>
2017-09-01 21:26:20 -0600

    Adding existing file

---
+++ basicQuestions.js
@@ -0,0 +1,14 @@
+module.exports.basicQuestions = [
+    {
+        front: 'What planet is the seventh planet from the Sun in our Solar System?',
+        back: 'Uranus'
+    },
+    {
+        front: 'Uranus, named after the god of the sky, comes from what language?',
+        back: 'Greek'
+    },
+    {
+        front: 'Which planet boasts 18 moons?',
+        back: 'Uranus'
+    },
+]
