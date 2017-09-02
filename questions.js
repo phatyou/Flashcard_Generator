199a661f9ab551158b5b5ad6e3fdbd692b2e27db
Jennifer Mitchell <jennifer.mitchell@utah.edu>
2017-09-01 21:32:51 -0600

    cloze-deleted questions file

---
+++ questions.js
@@ -0,0 +1,78 @@
+module.exports.questions = [
+	{
+		full: 'Uranus is the seventh planet from the Sun in our Solar System.',
+		cloze: 'seventh'
+	},
+	{
+		full: 'A year on Uranus takes 84.07 Earth years; it takes 84.07 Earth years for Uranus to orbit the Sun once.',
+		cloze: 'Sun'
+	},
+	{
+		full: 'Uranus was named for the ancient Greek god of the sky.',
+		cloze: 'Greek'
+	},
+	{
+		full: 'Uranus is encircled by a belt of 11 faint rings and 18 moons.',
+		cloze: 'moons'
+	},
+	{
+		full: 'Venus is the second planet from the Sun in our Solar System.',
+		cloze: 'second'
+	},
+	{
+		full: 'Venus does not have any moons or rings.',
+		cloze: 'rings'
+	},
+	{
+		full: 'A dense layer of clouds, made up of sulphuric acid, obscure the surface of Venus.',
+		cloze: 'clouds'
+	},
+    {
+        full: 'Venus spins slowly, in the opposite direction most planets do. This backwards rotation is known as retrograde.',
+        cloze: 'retrograde'
+    },
+	{
+		full: 'There are no seasons on Mercury.',
+		cloze: 'Mercury'
+	},
+	{
+		full: 'Instead of an atmosphere, Mercury possesses a thin exosphere made up of atoms blasted off the surface by solar wind and striking micrometeoroids.',
+		cloze: 'exosphere'
+	},
+	{
+		full: 'In 1631, Galileo observed Mercury with a new invention- the telescope.',
+		cloze: 'Galileo'
+	},
+	{
+		full: 'Caltech researchers have found evidence suggesting there may be a new object, nicknamed "Planet X" deep in the solar system.',
+		cloze: 'Planet X'
+	},
+	{
+		full: 'Saturn is the least dense planet in the Solar System.',
+		cloze: 'dense'
+	},
+	{
+		full: 'Saturn is the sixth planet from the Sun.',
+		cloze: 'sixth'
+	},
+	{
+		full: 'The planet Saturn is named after the Roman god of farming.',
+		cloze: 'farming'
+	},
+	{
+		full: 'The largest moon on Saturn is known as Titan.',
+		cloze: 'Titan'
+	},
+	{
+		full: 'Saturn’s moon Titan is the second-largest moon in the solar system. Only Jupiter’s moon Ganymede (named after one of Zeus’ lovers) is larger.',
+		cloze: 'Ganymede'
+	},
+	{
+		full: 'Saturn was discovered by Galileo in 1610.',
+		cloze: '1610'
+	},
+	{
+		full: 'Saturn has been known since prehistoric times because it is easily visible to the naked eye.',
+		cloze: 'Saturn'
+	}
+]
