08d5f6241360a62fad01d01dd9e954bde8dd227e
Jennifer Mitchell <jennifer.mitchell@utah.edu>
2017-09-01 21:25:27 -0600

    Add files via upload

---
+++ BasicCard.js
@@ -0,0 +1,9 @@
+// BasicCard constructor
+module.exports.BasicCard = function(front, back) {
+    // var frontToLower = front.toLowerCase();
+    var backToLower = back.toLowerCase();
+
+	this.front = front;
+	this.back = back;
+}
+console.log('Loaded')
